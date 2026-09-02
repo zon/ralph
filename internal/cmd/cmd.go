d438619c135a2cd85d10e3fc27725301bc43c583
Matthew Z Haralovich <zon@haralovich.org>
2026-09-02 14:26:08 -0400

    Reorder kong subcommands

--- internal/cmd/cmd.go
+++ internal/cmd/cmd.go
@@ -4,17 +4,17 @@ package cmd
 type Cmd struct {
 	// Subcommands
 	Run        RunCmd        `cmd:"" default:"withargs" help:"Execute Ralph with a project file (default command)"`
+	Loop       LoopCmd       `cmd:"" help:"Run AI iterations over a set of steps"`
 	Command    CommandCmd    `cmd:"" help:"Run a command in a remote Ralph workflow"`
-	Setup      SetupCmd      `cmd:"" help:"Configure credentials for remote execution"`
+	Validate   ValidateCmd   `cmd:"" help:"Validate a project YAML file"`
 	Complete   CompleteCmd   `cmd:"" help:"List the completion hashes recorded in the commit log of this branch"`
 	Incomplete IncompleteCmd `cmd:"" help:"List project items not complete in this branch"`
-	Workflow   WorkflowGroup `cmd:"" help:"Run Ralph workflow subcommands in a container"`
-	Validate   ValidateCmd   `cmd:"" help:"Validate a project YAML file"`
-	Help       HelpGroup     `cmd:"" help:"Show help for Ralph topics"`
 	List       ListCmd       `cmd:"" help:"List Argo workflows"`
 	Stop       StopCmd       `cmd:"" help:"Stop an Argo workflow"`
 	Logs       LogsCmd       `cmd:"" help:"Get logs of an Argo workflow"`
-	Loop       LoopCmd       `cmd:"" help:"Run AI iterations over a set of steps"`
+	Setup      SetupCmd      `cmd:"" help:"Configure credentials for remote execution"`
+	Workflow   WorkflowGroup `cmd:"" help:"Run Ralph workflow subcommands in a container"`
+	Help       HelpGroup     `cmd:"" help:"Show help for Ralph topics"`
 
 	version string `kong:"-"`
 	date    string `kong:"-"`
@@ -23,9 +23,9 @@ type Cmd struct {
 // WorkflowGroup defines the workflow subcommand group
 type WorkflowGroup struct {
 	Run     WorkflowRunCmd     `cmd:"" help:"Invoked by workflows to execute a project"`
+	Loop    WorkflowLoopCmd    `cmd:"" help:"Invoked by workflows to execute a loop"`
 	Command WorkflowCommandCmd `cmd:"" help:"Invoked by workflows to execute a command"`
 	Token   WorkflowTokenCmd   `cmd:"" help:"Invoked by workflows to configure git and gh auth"`
-	Loop    WorkflowLoopCmd    `cmd:"" help:"Invoked by workflows to execute a loop"`
 }
 
 // HelpGroup defines the help subcommand group
